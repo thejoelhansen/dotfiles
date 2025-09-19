695e70c11e982d75f5259ab56cd1b03440f92ec4
thejoelhansen <6098265+thejoelhansen@users.noreply.github.com>
2025-09-19 14:22:30 -0700

    update install.sh to include PATH variables to /bin/, add widta.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@ ln -is $(readlink -f ~/.dotfiles/bashrc) ~/.bashrc
 ln -is $(readlink -f ~/.dotfiles/vimrc) ~/.vimrc
 ln -is $(readlink -f ~/.dotfiles/build.log) ~/build.log
 ln -is $(readlink -f ~/.dotfiles/gitconfig) ~/.gitconfig
+
+var_check=$(cat ~/.bash_profile | grep "dotfiles/bin")
+if [ -z "$var_check" ]; then
+	echo 'export PATH="$HOME/.dotfiles/bin:$PATH"' >> ~/.bash_profile
+fi
